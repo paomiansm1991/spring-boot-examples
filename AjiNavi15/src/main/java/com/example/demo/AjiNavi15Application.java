c3ce5011fcd7cf783e65db7d61224ebf6328e57a
WISS1_0328 <WISS1_0328@PC0328>
2025-08-12 13:35:59 +0900

    AjiNavi14ApplicationをAjiNavi15Applicationに変更

--- AjiNavi15/src/main/java/com/example/demo/AjiNavi15Application.java
+++ AjiNavi15/src/main/java/com/example/demo/AjiNavi15Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AjiNavi14Application {
+public class AjiNavi15Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AjiNavi14Application.class, args);
+		SpringApplication.run(AjiNavi15Application.class, args);
 	}
 
 }
--- AjiNavi15/src/test/java/com/example/demo/AjiNavi15ApplicationTests.java
+++ AjiNavi15/src/test/java/com/example/demo/AjiNavi15ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AjiNavi14ApplicationTests {
+class AjiNavi15ApplicationTests {
 
 	@Test
 	void contextLoads() {
