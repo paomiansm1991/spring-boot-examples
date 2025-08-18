fc1f42777b68abf107a4f0933659caf6f622339e
WISS1_0328 <WISS1_0328@PC0328>
2025-08-18 16:53:40 +0900

    AjiNavi19ApplicationをAjiNavi20Applicationに名前変更

--- AjiNavi20/src/main/java/com/example/demo/AjiNavi20Application.java
+++ AjiNavi20/src/main/java/com/example/demo/AjiNavi20Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AjiNavi19Application {
+public class AjiNavi20Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AjiNavi19Application.class, args);
+		SpringApplication.run(AjiNavi20Application.class, args);
 	}
 
 }
--- AjiNavi20/src/test/java/com/example/demo/AjiNavi20ApplicationTests.java
+++ AjiNavi20/src/test/java/com/example/demo/AjiNavi20ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AjiNavi19ApplicationTests {
+class AjiNavi20ApplicationTests {
 
 	@Test
 	void contextLoads() {
