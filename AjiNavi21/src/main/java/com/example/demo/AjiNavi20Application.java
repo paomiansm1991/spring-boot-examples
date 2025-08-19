8099e8b0070c7842eb3bc86068ed678c208fdb85
WISS1_0328 <WISS1_0328@PC0328>
2025-08-19 17:22:44 +0900

    AjiNavi20をコピーしてAjiNavi21に作成

---
+++ AjiNavi21/src/main/java/com/example/demo/AjiNavi20Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjiNavi20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjiNavi20Application.class, args);
+	}
+
+}
