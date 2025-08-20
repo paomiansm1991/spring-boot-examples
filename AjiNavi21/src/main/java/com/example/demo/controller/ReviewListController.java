4398a11a2fb9f50897cec3e12e1cba1c3978ec1f
WISS1_0328 <WISS1_0328@PC0328>
2025-08-20 14:44:39 +0900

    レビュー一覧コントローラー改造

--- AjiNavi21/src/main/java/com/example/demo/controller/ReviewListController.java
+++ AjiNavi21/src/main/java/com/example/demo/controller/ReviewListController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -11,38 +9,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.Review;
 import com.example.demo.form.ReviewSearchForm;
+import com.example.demo.service.ReviewListService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ReviewListController {
 
+	private final ReviewListService service;
+	
 	@PostMapping("/search-review")
 	private String searchReview(
 			@ModelAttribute ReviewSearchForm form,
 			Model model) {
 		
-		List<Review> list = new ArrayList<Review>();
+		List<Review> list = service.findByRestaurantId(form.getRestaurantId());
 
-		//テストデータ
-		//--1--
-		Review review = new Review();
-		review.setReviewId(1);
-		review.setRestaurantId(1);
-		review.setUserId("user1");
-		review.setVisitDate(Date.valueOf("2027-07-01"));
-		review.setRating(3);
-		review.setComment("comment1");
-		list.add(review);
-		//--2--
-		review = new Review();
-		review.setReviewId(2);
-		review.setRestaurantId(1);
-		review.setUserId("user2");
-		review.setVisitDate(Date.valueOf("2027-07-02"));
-		review.setRating(4);
-		review.setComment("comment2");
-		list.add(review);
-		//テストデータ(おわり)
-		
 		if (list.size() > 0) {
 			model.addAttribute("reviewList", list);
 		}
